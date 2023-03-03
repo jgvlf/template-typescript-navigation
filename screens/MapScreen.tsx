2320e31b146306ec60fa7ef52545906de834c65c
jgvlf <joaogabrielvlf@gmail.com>
2023-03-03 10:48:34 -0300

    👌 IMPROVE: Render Markers of a var

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -9,6 +9,33 @@ export default function MapScreen() {
   const [latitude,setLatitude] = useState(-22.913947865776727)
   const [longitude,setLongitude] = useState(-47.068129919628475)
 
+  const locais = [
+    {
+      nome: "Escola e Faculdade de Tecnologia SENAI Roberto Mange",
+      latitude: -22.913947865776727,
+      longitude: -47.068129919628475,
+    },
+    {
+      nome: " CaP Robert BOSCH GmbH",
+      latitude: -22.896841081767903,
+      longitude: -47.11158354612385,
+    },
+    {
+      nome: "CASA",
+      latitude: -22.915585442926915,
+      longitude: -47.0532584596165,
+    },
+    {
+      nome: "Colégio Franciscano Ave Maria",
+      latitude: -22.912134108508837,
+      longitude: -47.05499400379461,
+    },
+    {
+      nome: "Discamp",
+      latitude: -22.91716546061756,
+      longitude: -47.05717002624316,
+    },
+  ]
   return (
     <View style={styles.container}>
      <MapView style={styles.map}
@@ -19,11 +46,20 @@ export default function MapScreen() {
         longitudeDelta: 0.003,
     }}
   >
-    <Marker coordinate={{ latitude : latitude , longitude : longitude }}>
+    {locais.map(({nome, latitude, longitude}) => (
+      <Marker key={nome} coordinate={{latitude: latitude, longitude: longitude}}>
+        <Callout>
+          <Text>
+            {nome}
+          </Text>
+        </Callout>
+      </Marker>
+    ))}
+    {/* <Marker coordinate={{ latitude : latitude , longitude : longitude }}>
       <Callout>
         <Text>SENAI</Text>
       </Callout>
-    </Marker>
+    </Marker> */}
 
   </MapView>
     </View>
